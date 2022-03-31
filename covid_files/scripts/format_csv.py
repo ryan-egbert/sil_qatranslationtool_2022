ad6535b5266529a803010797ad0031397fceae56
ryan-egbert <ryanegbert@aggiemail.usu.edu>
2022-03-31 14:28:40 -0400

    Added formatted csv files

--- covid_files/scripts/format_csv.py
+++ covid_files/scripts/format_csv.py
@@ -3,24 +3,30 @@ import os
 import pandas as pd
 from pathlib import Path
 
-path = "/Users/ryanegbert/Desktop/spring22/ip/app/covid_files/csv/"
-files = os.listdir(path)
+old_path = "/Users/ryanegbert/Desktop/spring22/ip/app/covid_files/old_csv/"
+new_path = "/Users/ryanegbert/Desktop/spring22/ip/app/covid_files/csv/"
+files = os.listdir(old_path)
 
 for file in files:
-    if Path(path + file).is_file():
-        with open(path + file, 'r') as f:
-            reader = csv.reader(f)
-            reader_list = list(reader)
-            langs = reader_list[0]
-            lang_dict = {}
-            for lang in range(len(langs)):
-                print(lang)
-                lang_dict[lang] = {'lang': langs[lang], 'sentences': []}
-            print(lang_dict)
-            for i in range(1,len(reader_list)):
-                for j in range(len(reader_list[i])):
-                    if j in lang_dict:
-                        lang_dict[j]['sentences'].append(reader_list[i][j])
+    if Path(old_path + file).is_file():
+        with open(new_path + 'formatted_' + file, 'w') as o:
+            with open(old_path + file, 'r') as f:
+                reader = csv.reader(f)
+                reader_list = list(reader)
+                langs = reader_list[0]
+                lang_dict = {}
+                for lang in range(len(langs)):
+                    lang_dict[lang] = {'lang': langs[lang], 'sentences': []}
+                for i in range(1,len(reader_list)):
+                    for j in range(len(reader_list[i])):
+                        if j in lang_dict:
+                            lang_dict[j]['sentences'].append(reader_list[i][j])
 
-            print(lang_dict)
-            break
+                o.write('source,translated,question,answer\n')
+                try:
+                    for i in range(len(lang_dict[0]['sentences'])):
+                        s_sen = lang_dict[0]['sentences'][i]
+                        t_sen = lang_dict[1]['sentences'][i]
+                        o.write(f'"{s_sen}","{t_sen}",,\n')
+                except KeyError:
+                    print(f'File {file} is not compatible.')
